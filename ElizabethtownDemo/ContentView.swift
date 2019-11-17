3693bff0560c5cf8df277caa6e5145f0b51e90c3
Ben Johnson <code@benrobjoh.com>
2019-11-17 11:08:17 -0500

    Initialize Xcode project

---
+++ ElizabethtownDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElizabethtownDemo
+//
+//  Created by Ben Johnson on 11/17/19.
+//  Copyright © 2019 Ben Johnson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
